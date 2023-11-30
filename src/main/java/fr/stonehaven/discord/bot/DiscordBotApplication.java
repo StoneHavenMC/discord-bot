56193db74b17962127d2634eb68074d5465b2725
Léo Moriceau <thisis41@protonmail.com>
2023-11-30 15:29:41 +0100

    Project setup

---
+++ src/main/java/fr/stonehaven/discord/bot/DiscordBotApplication.java
@@ -0,0 +1,13 @@
+package fr.stonehaven.discord.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordBotApplication.class, args);
+    }
+
+}
