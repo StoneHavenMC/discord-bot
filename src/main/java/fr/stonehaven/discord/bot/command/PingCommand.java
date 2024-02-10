d87711fb20590b729b6bc9a60e382b099e72a654
Léo Moriceau <thisis41@protonmail.com>
2024-02-10 23:37:19 +0100

    WIP

--- src/main/java/fr/stonehaven/discord/bot/command/PingCommand.java
+++ src/main/java/fr/stonehaven/discord/bot/command/PingCommand.java
@@ -1,25 +1,19 @@
 package fr.stonehaven.discord.bot.command;
 
-import fr.stonehaven.discord.bot.dto.api.gitbook.search.GitbookAnswer;
-import fr.stonehaven.discord.bot.service.gitbook.IGitbookService;
 import jakarta.annotation.PostConstruct;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.time.OffsetDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -45,9 +39,9 @@ public class PingCommand extends ListenerAdapter {
 
         EmbedBuilder builder = new EmbedBuilder();
         builder.setTitle("**Ping**");
-            builder.setColor(Color.GRAY);
-            long ping = Timestamp.from(Instant.now()).compareTo(Timestamp.valueOf(e.getTimeCreated().toLocalDateTime()));
-            builder.setDescription("Le ping du bot est actuellement de " + ping + " ms");
+        builder.setColor(Color.GRAY);
+        long ping = Timestamp.from(Instant.now()).compareTo(Timestamp.valueOf(e.getTimeCreated().toLocalDateTime()));
+        builder.setDescription("Le ping du bot est actuellement de " + ping + " ms");
         e.getHook().sendMessageEmbeds(builder.build()).queue();
     }
 
